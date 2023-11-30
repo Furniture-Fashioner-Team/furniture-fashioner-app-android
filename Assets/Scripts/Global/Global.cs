aecbd491938d1ef8c3f59bb76e5b9268bdfb7897
Jussi_Latikka <jussilatikka@gmail.com>
2023-11-30 23:41:22 +0200

    Menu icon place adjusted

--- Assets/Scripts/Global/Global.cs
+++ Assets/Scripts/Global/Global.cs
@@ -37,7 +37,7 @@ class Global
         GlobalMenu.imgScaleFact = GlobalMenu.scrollViewSize.y * 0.22f;
         GlobalMenu.spriteImageSize = new Vector2(1.920f * GlobalMenu.imgScaleFact, 1.080f * GlobalMenu.imgScaleFact);
         GlobalARC.iconSize = new Vector2(dim[0] * 0.1042f, dim[1] * 0.0485f);
-        GlobalARC.menuIconPlace = new Vector2(dim[0] * -0.434f, dim[1] * 0.474f);
+        GlobalARC.menuIconPlace = new Vector2(dim[0] * -0.399f, dim[1] * 0.461f);
         GlobalARC.trashCanPlace = new Vector2(dim[0] * 0.399f, dim[1] * -0.461f);
         GlobalARC.duplicatePlace = new Vector2(dim[0] * -0.399f, dim[1] * -0.461f);
         GlobalARC.newObjPlace = new Vector3(0, 0, 3);
